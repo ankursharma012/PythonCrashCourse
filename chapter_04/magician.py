0ad61d0c6ef17f45bdc36a5b2f61de6c492286fe
Ankur Sharma <ankur.sharma@ubank.com.au>
2024-12-04 16:36:03 +1100

    Magician

---
+++ chapter_04/magician.py
@@ -0,0 +1,4 @@
+magicians = ['ankur', 'sharma', 'manoj']
+for magician in magicians:
+    print(f"{magician.title()} is the magician")
+print(f"I am looking forward to your next trick {magician.upper()}")
