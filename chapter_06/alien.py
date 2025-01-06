2606177f610fa2cc3553c207e95ccec4f808ec1d
Ankur Sharma <ankur.sharma@ubank.com.au>
2025-01-06 16:04:53 +1100

    Add

--- chapter_06/alien.py
+++ chapter_06/alien.py
@@ -1,2 +1,14 @@
-alien_0 = {'color':'green', 'points':5}
-print(f"The alien color is: {alien_0['color']} and points are {alien_0['points']}")
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed':'fast'}
+print(f"The original position for the alien is: {alien_0['x_position']}")
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else :
+    x_increment = 3
+
+alien_0['x_position'] += x_increment
+
+del alien_0['speed']
+print(f"The new position is {alien_0}")
