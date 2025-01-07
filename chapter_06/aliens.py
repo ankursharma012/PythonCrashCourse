ab665fd8923fc95845b7b1a90cfe2fda626d3d67
Ankur Sharma <ankur.sharma@ubank.com.au>
2025-01-08 09:51:11 +1100

    test

--- chapter_06/aliens.py
+++ chapter_06/aliens.py
@@ -1,15 +1,9 @@
-aliens = []
+pizza = {
+    'crust':'thick',
+    'toppings':['mushroom', 'chicken']
+}
 
-for alien_number in range(30):
-    new_alien = {"color":"green", "points":alien_number}
-    aliens.append(new_alien)
-
-for alien in aliens[:3]:
-    if alien['color'] == 'green':
-        alien['color'] = 'yellow'
-        alien['points'] = 10
-
-for alien in aliens[:5]:
-    print(alien)
-
-print(f"Total number of aliens are: {len(aliens)}")
+print(f"You ordered a {pizza['crust']}-crust pizza"
+      "with the toppings:")
+for topping in pizza['toppings']:
+    print(f"\t{topping}")
